2465311a5e20a3037fa5b2686158626b06340139
zxcShadee <98111819+zxcShadee@users.noreply.github.com>
2026-06-05 23:51:49 +0300

    Update test_chip8.cpp

--- tests/test_chip8.cpp
+++ tests/test_chip8.cpp
@@ -9,7 +9,6 @@ TEST_CASE("Chip8 Initialization") {
     CHECK(chip.I == 0);
     CHECK(chip.delayTimer == 0);
     
-    // Проверка загрузки шрифта (первый байт нуля)
     CHECK(chip.memory[0x50] == 0xF0);
 }
 
@@ -21,7 +20,6 @@ TEST_CASE("Chip8 loadRom Function") {
     }
 
     SUBCASE("Positive: Loading valid dummy ROM") {
-        // Создаем временный файл
         const std::string testPath = "test_rom.ch8";
         std::ofstream testFile(testPath, std::ios::binary);
         uint8_t dummyData[] = {0xA2, 0xF0, 0x12, 0x34}; // LD I, 0x2F0; JP 0x234
@@ -42,23 +40,23 @@ TEST_CASE("Chip8 emulateCycle Function") {
     Chip8 chip;
 
     SUBCASE("Positive: Instruction 0x00E0 (Clear Screen)") {
-        chip.display.fill(0xFF); // Заполняем экран мусором
+        chip.display.fill(0xFF); 
         chip.memory[0x200] = 0x00;
         chip.memory[0x201] = 0xE0;
         
         REQUIRE_NOTHROW(chip.emulateCycle());
         
-        // Экран должен быть очищен
+        
         bool allClear = true;
         for (auto pixel : chip.display) {
             if (pixel != 0) allClear = false;
         }
         CHECK(allClear == true);
-        CHECK(chip.pc == 0x202); // PC увеличился на 2
+        CHECK(chip.pc == 0x202); 
     }
 
     SUBCASE("Negative: Unknown Instruction") {
-        chip.memory[0x200] = 0xFF; // Несуществующий опкод
+        chip.memory[0x200] = 0xFF; 
         chip.memory[0x201] = 0xFF;
         
         CHECK_THROWS_AS(chip.emulateCycle(), std::runtime_error);
